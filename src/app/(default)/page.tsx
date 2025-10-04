2f49c89fdaf781cc37a74d09c672fed15ed6070d
Ivorra-1996 <ivorrajosemaria@gmail.com>
2025-10-03 23:09:12 -0300

    se creó HomePage

--- src/app/(default)/page.tsx
+++ src/app/(default)/page.tsx
@@ -1,9 +1,11 @@
+import Homepage from '@/components/Home/Homepage';
 import { Navbar } from '@/components/Navbar';
 
 export default function Home() {
   return (
     <div>
       <Navbar />
+      <Homepage />
     </div>
   );
 }
