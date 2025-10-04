f7c4af72901719a8feb4bae5fbe43a26fdec9407
Ivorra-1996 <ivorrajosemaria@gmail.com>
2025-10-03 22:46:44 -0300

    Reestructurar Navbar y estilos, renombrar clases y mejorar la legibilidad del código

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -5,16 +5,18 @@ import style from './Navbar.module.css';
 
 const Navbar: React.FC = () => {
   return (
-    <div className={style.navbar}>
-      <div className={style.icon_title}>
-        <EyeSauron />
-        <p className={style.font_title}>Mordor Portfolio</p>
-      </div>
-      <div className={style.options}>
-        <p>Home</p>
-        <p>About</p>
-        <p>Projects</p>
-        <p>Contact</p>
+    <div className={style.navbarContainer}>
+      <div className={style.descriptionContainer}>
+        <div className={style.icon_title}>
+          <EyeSauron />
+          <p className={style.font_title}>Mordor Portfolio</p>
+        </div>
+        <div className={style.options}>
+          <p>Home</p>
+          <p>About</p>
+          <p>Projects</p>
+          <p>Contact</p>
+        </div>
       </div>
     </div>
   );
