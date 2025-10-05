9db6bfe733e5203f594f096a799712e3db33f545
Ivorra-1996 <ivorrajosemaria@gmail.com>
2025-10-05 19:33:39 -0300

    Creación de componente Home con sus estilos

--- src/components/Home/Homepage.tsx
+++ src/components/Home/Homepage.tsx
@@ -1,5 +1,20 @@
+import style from './Home.module.css';
+
 const Homepage = () => {
-  return <div>Homepage</div>;
+  return (
+    <div className={style.homepageContainer}>
+      <div className={style.imageContainer}>
+        <img src='Sauron.png' alt='Sauron dibujo en papel viejo' width={880} />
+        <div className={style.textContainer}>
+          <div className={style.presentation}>
+            <h1>Ivorra Jose</h1>
+            <p>Desarrollador Frontend | React | Next.js | JavaScript | TypeScript</p>
+            <button>Ver proyectos</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Homepage;
