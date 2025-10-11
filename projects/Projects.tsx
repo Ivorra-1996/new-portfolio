7579e3ff6c100df90622e106e77eda3ced3aa9d3
Ivorra-1996 <ivorrajosemaria@gmail.com>
2025-10-11 00:54:39 -0300

    mock de proyectos

---
+++ projects/Projects.tsx
@@ -0,0 +1,13 @@
+const proyectos = [
+  {
+    title: 'Lavado de manos con IA',
+    category: 'Frontend',
+    image: '/Lavado de manos.png',
+    description:
+      'Demo del proceso de lavado de manos implementado con un modelo de inteligencia artificial, el cual reconoce y supervisa cada uno de los pasos hasta completar correctamente el procedimiento.',
+    technology: ['NextJs', 'Tsx', 'CSS', 'React', 'TensorFlow', 'YOLO', 'React webcam', 'Node.js', 'Yarn'],
+    link: 'https://github.com/ADOX-SA/lavadodemanos_demo',
+  },
+];
+
+export default proyectos;
