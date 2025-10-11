7fbbebeb2c40bd091dbf9632960acac0aa0c1516
Ivorra-1996 <ivorrajosemaria@gmail.com>
2025-10-11 00:53:58 -0300

    nuevo componente

--- src/components/Projects/Projects.tsx
+++ src/components/Projects/Projects.tsx
@@ -1,28 +1,63 @@
+'use client';
+import { useState } from 'react';
+import proyectos from '../../../projects/Projects';
+// import { NotProjects } from './components/NotProjects';
+import { FaRegEye } from 'react-icons/fa';
+import { Paragraph } from './/Paragraph/index';
+import { Images } from './Image';
 import style from './Projects.module.css';
 
 const Projects = () => {
+  const [currentCategory, setCategory] = useState('Todo');
+
+  const filteredProjects = proyectos.filter((project) => {
+    const matchCategory = currentCategory === 'Todo' || project.category === currentCategory;
+    return matchCategory;
+  });
+
   return (
-    <div className={style.contentProjects}>
-      <div className={style.descriptionProjects}>
+    <div className={style.projectsContainer}>
+      <div className={style.descriptionContainer}>
         <div className={style.title}>
-          <h2>My Projects</h2>
+          <h2>Projects</h2>
         </div>
-        <section className={style.layout}>
-          <div className={style.projectCard}>
-            <img src='Sauron.png' alt='Sauron dibujo en papel viejo' />
-            <h3>Lavado de manos</h3>
-            <p>Este es un proyectos de lavados de manos que sirve para mejorar la trazabilidad y control del lavado.</p>
-          </div>
-          <div>2</div>
-          <div>3</div>
-          <div>4</div>
-          <div>5</div>
-          <div>6</div>
-          <div>7</div>
-          <div>8</div>
-          <div>9</div>
-          <div>9</div>
-        </section>
+        <div>
+          <Paragraph currentCategory={currentCategory} setCategory={setCategory} />
+          {filteredProjects.length > 0 ? (
+            <div className={style.projectList}>
+              {filteredProjects.map((project, index) => (
+                <div key={index} className={`${style.projectItem} ${style.active}`}>
+                  <a href={project.link}>
+                    <div className={style.projectImg}>
+                      <div className={style.projectItemIconBox}>
+                        <FaRegEye name='eye' />
+                      </div>
+                      <Images src={project.image} alt={project.title} />
+                    </div>
+                    <h2 className={style.projectTitle}>{project.title}</h2>
+                    <p className={style.projectDescription}>{project.description}</p>
+                    <div className={style.content}>
+                      {Array.isArray(project.technology) ? (
+                        project.technology.map((tech, techIndex) => (
+                          <p key={techIndex} className={style.technology}>
+                            {tech}
+                          </p>
+                        ))
+                      ) : (
+                        <p className={style.technology}>{project.technology}</p>
+                      )}
+                    </div>
+                    <p className={style.projectCategory}>{project.category}</p>
+                  </a>
+                </div>
+              ))}
+            </div>
+          ) : (
+            // <NotProjects />
+            <p>no hay proyectos</p>
+          )}
+          <hr />
+        </div>
       </div>
     </div>
   );
